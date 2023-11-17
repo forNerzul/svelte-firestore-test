fa161637e21a62b012bc3562a36827a9fe433a4e
forNerzul <sbeardman92@gmail.com>
2023-11-17 13:18:17 -0300

    moviendo los archivos al directorio lib

---
+++ src/lib/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA9wvepQYbbmM5BrUa31EsB-_qxBBhFCIU",
+    authDomain: "svelte-firestore-test.firebaseapp.com",
+    projectId: "svelte-firestore-test",
+    storageBucket: "svelte-firestore-test.appspot.com",
+    messagingSenderId: "731259203514",
+    appId: "1:731259203514:web:93939da0cbc02d3be1f783",
+    measurementId: "G-BQMV55RQ1B"
+  }
